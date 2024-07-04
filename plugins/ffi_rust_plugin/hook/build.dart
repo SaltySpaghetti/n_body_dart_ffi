f36b0e545f834c2f0297b5e076f24497e7d3a36b
Matej Knopp <matej.knopp@gmail.com>
2024-07-04 20:55:08 +0200

    use native_toolchain_rust

--- plugins/ffi_rust_plugin/hook/build.dart
+++ plugins/ffi_rust_plugin/hook/build.dart
@@ -5,42 +5,21 @@
 import 'dart:io';
 
 import 'package:native_assets_cli/native_assets_cli.dart';
+import 'package:native_toolchain_rust/native_toolchain_rust.dart';
 
 void main(List<String> args) async {
   await build(args, (config, output) async {
-    final target = config.targetArchitecture == Architecture.arm64
-        ? 'aarch64-apple-darwin'
-        : 'x86_64-apple-darwin';
-    final result = await Process.run(
-      'cargo',
-      [
-        'build',
-        '--release',
-        '--target=$target',
-        '--target-dir=${config.outputDirectory.toFilePath()}'
-      ],
-      workingDirectory: config.packageRoot.resolve('src/rust/').toFilePath(),
+    final builder = RustBuilder(
+      package: 'ffi_rust_plugin',
+      cratePath: 'src/rust',
+      // Specify custom name to match the dart file, otherwise crate name is used.
+      assetName: 'ffi_rust_plugin_bindings_generated.dart',
+      buildConfig: config,
+      useNativeManifest: false,
     );
-
-    if (result.exitCode != 0) {
-      print('${result.stderr} ${result.stdout}');
-      throw 'cargo build failed';
-    }
-    // TODO: use config.outputDirectory and tell to cargo.
-    output.addAsset(
-      NativeCodeAsset(
-        package: 'ffi_rust_plugin',
-        name: 'ffi_rust_plugin_bindings_generated.dart',
-        linkMode: DynamicLoadingBundled(),
-        os: config.targetOS,
-        architecture: config.targetArchitecture,
-        file: config.outputDirectory
-            .resolve('$target/release/libn_body_rust_simulation.dylib'),
-      ),
-    );
+    await builder.run(output: output);
     output.addDependencies([
       config.packageRoot.resolve('hook/build.dart'),
     ]);
-    // TODO output.addDependencies
   });
 }
